993822993523e96f184cc421972cd9c48b464dc0
Baihaqi43 <boihaki43@gmail.com>
2021-12-12 14:24:05 +0700

    membuat halaman profile dan kumpul file

---
+++ profile.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rabithah Thaliban Aceh</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+</head>
+
+<body style="background-color:#23651d">
+    <div class="first">
+        <?php include_once("element/header.php"); ?>
+    </div>
+    <div class="second">
+    </div>
+  <h1 class="text-center my-5 " style="font-weight: 600; color:white">Selamat Datang User</h1>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-12 h-100 py-4" style="max-width: 700px;
+        position: relative;
+          background: rgba(248, 248, 248, 0.2);
+            border: 3px solid #FFFFFF;
+            box-sizing: border-box;
+            box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+            backdrop-filter: blur(70px);
+            /* Note: backdrop-filter has minimal browser support */
+            border-radius: 30px;">
+            <div class="d-flex justify-content-evenly">
+              <img width="150px" height="150px" src="docs/user.png" class="rounded-circle my-auto" alt="user" style="object-fit: cover; object-position:center top;">
+              <div class="my-auto" style="
+font-weight: semi-bold;
+font-size: 20px;
+line-height: 30px;
+
+color: #FFFFFF;">
+               <table>
+                 <tr>
+                   <td>No. KTA</td>
+                   <td width="30px"></td>
+                   <td>: 0000</td>
+                 </tr>
+                 <tr>
+                   <td>Nama</td>
+                   <td width="30px"></td>
+                   <td>: Baihaqi</td>
+                 </tr>
+                 <tr>
+                   <td>Status</td>
+                   <td width="30px"></td>
+                   <td>: Aktif</td>
+                 </tr>
+                 <tr>
+                   <td>Dapartement</td>
+                   <td width="30px"></td>
+                   <td>: Kerohanian</td>
+                 </tr>
+
+               </table>
+              </div>
+            </div>
+            
+      </div>
+    </div>
+    <h1 class="text-center my-5 " style="font-weight: 600; color:white">Edit Profile</h1>
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 " style="
+        position: relative;
+        max-width: 700px;
+        height: 586px;
+          background: rgba(248, 248, 248, 0.2);
+            border: 3px solid #FFFFFF;
+            box-sizing: border-box;
+            box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+            backdrop-filter: blur(70px);
+            /* Note: backdrop-filter has minimal browser support */
+            border-radius: 30px;">
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="footer mt-5">
+        <h6>
+            &#169 2021 Copyright by <a href="" style="color:inherit">Space.devs</a>
+        </h6>
+    </div>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
